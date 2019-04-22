fd834cda4f072b6dd0f04cc96120200d201dc64e
Tyler Kidd <tylerckiddcontact@gmail.com>
2019-04-22 04:12:05 -0400

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+var friends = [
+    
+        {
+            name: "cory",
+            photo: "https://assets.teenvogue.com/photos/5c66f5aae78cdb389d7a3138/16:9/w_1280,c_limit/hero-spongebob-squarepants.jpg",
+            preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+          },
+      
+	
+];
+
+module.exports = friends;
